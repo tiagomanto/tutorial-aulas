90f0f6e015d547ac35b19d58d7b77d5149e338f0
Tiago Alexandre Mantovani <tiagoservodejesus@hotmail.com>
2021-02-25 11:04:32 -0300

    Tirar errio

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
             <h3>Microsoft Word &rarr;</h3>
               <h4>Alterando as Margens do Documento</h4>
                 <ol>
-                  <li>Clique na Guia Exibir - Procure a caixa de seleção Régua</li>
+                  <li>Clique na Guia Exibir -Procure a caixa de seleção Régua</li>
                   <img width="100%"  src="https://s2.glbimg.com/V7F3yJvg1kcoMD56LHDHdR0aM0w=/0x0:695x370/600x0/smart/filters:gifv():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2017/Y/J/JuDVFHRqOqzPoMmkfWJQ/captura-de-tela-2-.jpg" alt=""/>
               
                   <img width="100%" src="https://s2.glbimg.com/R3n3GSgeLGEXO48zIt3t9P9BT_Y=/0x0:695x370/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2017/9/z/snppnWRue2cfnbYXsXTw/captura-de-tela-4-.jpg" alt=""/>
